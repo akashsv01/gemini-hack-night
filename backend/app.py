7e005b9ad4ca9a4b9c1c76ce17e51b03faa12455
drewnicorn15 <drewdietrich1@gmail.com>
2025-11-02 13:49:04 -0500

    step 1 in pipeline: identify top 3 things from image

--- backend/app.py
+++ backend/app.py
@@ -2,17 +2,21 @@ from fastapi import FastAPI
 from google import genai
 import os
 from dotenv import load_dotenv
-
+from pydantic import BaseModel
+from PIL import Image
 app = FastAPI()
 load_dotenv()
 
 client = genai.Client(api_key=os.environ["GEMINI_API_KEY"])
-
-
-@app.get("/testprompt/{prompt}")
-async def generate(prompt: str):
+def identify_food(dishIMG):
+    class Response(BaseModel):
+        options: list[str]
     response = client.models.generate_content(
-        model="gemini-2.5-flash", contents=prompt
+        model="gemini-2.5-flash", contents=[f"what is the top 3 most likely dishes in the provided image",dishIMG],
+        config={
+        "response_mime_type": "application/json",
+        "response_schema": Response,
+    }
     )
     print(response.text)
     return {"response_text": response.text}
